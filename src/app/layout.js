de59f273f51488eadfe4daa6569be93922eb034b
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-09-21 20:08:14 +0600

    Google Font implement

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className={`${geistMono.className} min-h-full flex flex-col`}>
         {/* <h2>Fixed Portion</h2> */}
 
         <Navbar />
